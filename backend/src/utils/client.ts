a23e85569d81cb720f302e2af3afed276df5a7ce
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2025-05-20 02:25:18 +0530

    chore: Update client configuration to use mainnet environment and adjust Alchemy API endpoints for mainnet

--- backend/src/utils/client.ts
+++ backend/src/utils/client.ts
@@ -1,28 +1,28 @@
-import { PublicClient, testnet } from "@lens-protocol/client";
+import { PublicClient, mainnet } from "@lens-protocol/client";
 import { createPublicClient, createWalletClient, http } from "viem";
-import { lensTestnet } from "viem/chains";
+import { lens } from "viem/chains";
 import dotenv from "dotenv";
 import { privateKeyToAccount } from "viem/accounts";
 dotenv.config();
 
 export const client = PublicClient.create({
-  environment: testnet,
-  origin: "http://localhost:4000",
+  environment: mainnet,
+  origin: "https://clash-of-lens.onrender.com",
 });
 
 export const publicClient = createPublicClient({
-  chain: lensTestnet,
+  chain: lens,
   transport: http(
-    `https://lens-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
+    `https://lens-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
   ),
 });
 
 const account = privateKeyToAccount(process.env.PRIVATE_KEY as `0x${string}`);
 
 export const walletClient = createWalletClient({
-  chain: lensTestnet,
+  chain: lens,
   transport: http(
-    `https://lens-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
+    `https://lens-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`
   ),
   account,
 });
